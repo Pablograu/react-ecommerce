1ebdd5d55c824261d32aa1563c4a57d13a74f033
Pablograu <paugf89@gmail.com>
2019-08-21 20:01:29 +0200

    routes done

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello from navbar</h3>
+      </div>
+    )
+  }
+}

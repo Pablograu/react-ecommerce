9f7a909d4a8ebe61514d624f5f4c9c5704a039d5
Pablograu <paugf89@gmail.com>
2019-09-18 12:00:43 +0200

    addes total methods

--- src/context.js
+++ src/context.js
@@ -9,7 +9,7 @@ class ProductProvider extends Component {
   state = {
     products: [],
     detailProduct: detailProduct,
-    cart: storeProducts,
+    cart: [],
     modalOpen: false,
     modalProduct: detailProduct,
     cartSubTotal: 0,
@@ -57,7 +57,7 @@ class ProductProvider extends Component {
         return { products: tempProducts, cart: [...this.state.cart, product] };
       },
       () => {
-        console.log(this.state);
+        this.addTotals()
       }
     );
   };
@@ -84,6 +84,20 @@ class ProductProvider extends Component {
   }
   clearCart = () => {
     console.log('cart was cleared')
+  }
+  addTotals = () => {
+    let subTotal = 0
+    this.state.cart.map(item => (subTotal += item.total))
+    const tempTax = subTotal * 0.2
+    const tax = parseFloat(tempTax.toFixed(2))
+    const total = subTotal + tax
+    this.setState(() => {
+      return {
+        cartSubTotal: subTotal,
+        cartTax: tax,
+        cartTotal: total
+      }
+    })
   }
   render() {
     return (

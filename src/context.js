017ffed61dc7b30eafb6b7a98c30237af1232ea1
Pablograu <paugf89@gmail.com>
2019-09-18 15:58:25 +0200

    added removeItem method

--- src/context.js
+++ src/context.js
@@ -80,10 +80,35 @@ class ProductProvider extends Component {
     console.log('this is decrement method')
   }
   removeItem = (id) => {
-    console.log('item removed')
+    let tempProducts = [...this.state.products]
+    let tempCart = [...this.state.cart]
+
+    tempCart =tempCart.filter(item => item.id !== id)
+
+    const index = tempProducts.indexOf(this.getItem(id))
+    let removedProduct = tempProducts[index]
+    removedProduct.inCart = false
+    removedProduct.count = 0
+    removedProduct.total = 0
+    this.setState(() => {
+      return {
+        cart: [...tempCart],
+        products: [...tempProducts]
+      }
+    }, () => {
+      this.addTotals()
+    })
   }
+
   clearCart = () => {
-    console.log('cart was cleared')
+    this.setState(() => {
+      return {
+        cart: [],
+      }
+    }, ()=> {
+      this.setProducts()
+      this.addTotals()
+    })
   }
   addTotals = () => {
     let subTotal = 0
